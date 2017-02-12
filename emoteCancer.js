c5a479ca8524ce5b0902642e83307c878cd9369a
Dylan Walseth <dylanwalseth@gmail.com>
2017-02-12 00:57:37 -0600

    More DYNAMIC

--- emoteCancer.js
+++ emoteCancer.js
@@ -1,4 +1,6 @@
-var mappings = {
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+mappings = {
     "a": ":abc:",
     "b": ":csgob:",
     "c": ":carbon:",
@@ -27,11 +29,15 @@ var mappings = {
     "z": ":NZA2_Zed:"
 }
 
-emoteMap=(str)=>str.toLowerCase().split("").map((x)=>mappings[x]!==undefined?mappings[x]:x).join("")
+emoteMap=(str)=>str.toLowerCase().split("").map((x)=>mappings[x]!==undefined?mappings[x]:x).join("");
 inputChanged=()=>{
     var input = document.getElementById("input");
     var output = document.getElementById("output");
+    for (var letter in letters) {
+        mappings[letters[letter]] = document.getElementById(letters[letter]).value;
+    }
     output.value = emoteMap(input.value);
+    writeMappingToTextArea();
 };
 
 outputClicked=()=>{
@@ -43,4 +49,29 @@ outputClicked=()=>{
     } catch (err) {
         alert('Oops, unable to copy');
     }
+};
+
+setInputMappings=()=>{
+    for (var letter in letters) {
+        document.getElementById(letters[letter]).value = mappings[letters[letter]] || "";
+    }
 }
+
+mappingChanged=()=>{
+    var mapping = document.getElementById("mapping");
+    try {
+        mapping = JSON.parse(mapping.value);
+        mappings = mapping
+        for (var letter in letters) {
+            document.getElementById(letters[letter]).value = mappings[letters[letter]];
+        }
+        inputChanged();
+    } catch (e) {
+        console.log("Error parsing json")
+    }
+}
+writeMappingToTextArea=()=>document.getElementById("mapping").value = JSON.stringify(mappings);
+document.addEventListener('DOMContentLoaded', function() {
+    writeMappingToTextArea();
+    setInputMappings();
+});
